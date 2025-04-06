045108a47f4cc9df9aa3043cf89242b15b9a423e
songyanlong <18625986716@163.com>
2025-04-06 23:09:38 +0800

    feat(blog): 修改md文件,增加组件使用方法

--- README.md
+++ README.md
@@ -10,15 +10,46 @@ npm install react-native-chart-tools
 
 ## Usage
 
+### 基本使用
 
 ```js
-import { multiply } from 'react-native-chart-tools';
+import { Echarts } from 'react-native-chart-tools';
 
-// ...
+function MyChart() {
+  const option = {
+    title: { text: '示例图表' },
+    xAxis: { data: ['A', 'B', 'C', 'D', 'E'] },
+    yAxis: {},
+    series: [{ type: 'bar', data: [5, 20, 36, 10, 10] }],
+  };
 
-const result = await multiply(3, 7);
+  return <Echarts option={option} style={{ height: 300 }} />;
+}
 ```
 
+### 示例图表
+
+![示例图表](./example/assets/chart-example.png)
+
+### 配置选项
+
+| 属性          | 类型      | 说明               |
+| ------------- | --------- | ------------------ |
+| option        | object    | ECharts配置对象    |
+| style         | ViewStyle | 容器样式           |
+| height        | number    | 图表的高度         |
+| onChartsError | function  | 图表加载失败时调用 |
+
+其他参数兼容webview的属性，可自行配置.
+https://github.com/react-native-webview/react-native-webview/blob/master/docs/Getting-Started.md
+
+### 兼容性
+
+| 平台    | 支持情况 | 备注                                                                                                                                                                                                  |
+| ------- | -------- | ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| Android | ✅       |
+| iOS     | ✅       |
+| 鸿蒙    | ✅       | 该组件基于webview实现，使用项目中安装的 react-native-webview,如果要适配鸿蒙,需要自行将react-native-webview鸿蒙化,适配方式参考:https://gitee.com/openharmony-sig/ohos_react_native/tree/master/docs/en |
 
 ## Contributing
 
--- src/Echarts/index.tsx
+++ src/Echarts/index.tsx
@@ -1,13 +1,16 @@
 import React, { useCallback, useEffect, useRef } from 'react';
-import { WebView, type WebViewMessageEvent } from 'react-native-webview';
+import {
+  WebView,
+  type WebViewMessageEvent,
+  type WebViewProps,
+} from 'react-native-webview';
 
-interface EchartsProps {
+interface EchartsProps extends WebViewProps {
   option: Record<string, any>; // Echarts 配置项
-  width?: number | string; // Echarts 宽度
-  height: number | string; // Echarts 高度
+  width?: number; // Echarts 宽度
+  height: number; // Echarts 高度
   style?: object; // webview 样式
-  onLoad?: (status: string) => void; // 加载完成回调
-  onError?: (status: string) => void; // 加载错误回调
+  onChartsError?: (error: string) => void; // 错误回调
 }
 
 const Echarts = (props: EchartsProps) => {
@@ -21,15 +24,13 @@ const Echarts = (props: EchartsProps) => {
         window.OptionUpdate = new CustomEvent('OptionUpdate', {
           detail: {
               option: ${JSON.stringify(props?.option)},
-              width: ${props?.width},
-              height: ${props?.height},
           },
         });
         window.dispatchEvent(window.OptionUpdate);
       `;
       webViewRef.current.injectJavaScript(injectedScript);
     }
-  }, [props?.option, props?.width, props?.height]);
+  }, [props?.option]);
 
   useEffect(() => {
     if (JSON.stringify(props?.option) !== JSON.stringify(option.current)) {
@@ -50,18 +51,16 @@ const Echarts = (props: EchartsProps) => {
   }, []);
 
   const handleOnMessage = (event: WebViewMessageEvent) => {
-    console.log('event', event.nativeEvent.data);
     try {
       const data = JSON.parse(event.nativeEvent.data);
       if (data.code === 1) {
         isLoading.current = true;
         loadEcharts();
-        props?.onLoad?.('脚本加载完成');
       } else if (data.code === 0) {
-        props?.onError?.('脚本加载失败');
+        props?.onChartsError?.('脚本加载失败');
       }
     } catch (error) {
-      props?.onError?.('消息解析失败');
+      props?.onChartsError?.('消息解析失败');
     }
   };
 
@@ -70,13 +69,19 @@ const Echarts = (props: EchartsProps) => {
       ref={webViewRef}
       scrollEnabled={false} // 禁用滚动
       originWhitelist={['*']}
-      source={require('./template.html')} // 调整路径到你的 HTML 文件
-      // eslint-disable-next-line react-native/no-inline-styles
-      style={{ flex: 1, ...(props?.style || {}) }}
-      onMessage={handleOnMessage}
       onError={() => {
         console.error('WebView加载错误');
       }}
+      {...props}
+      source={require('./template.html')} // 调整路径到你的 HTML 文件
+      // eslint-disable-next-line react-native/no-inline-styles
+      style={{
+        flex: 1,
+        height: props?.height || '100%',
+        width: props?.width || '100%',
+        ...(props?.style || {}),
+      }}
+      onMessage={handleOnMessage}
     />
   );
 };
